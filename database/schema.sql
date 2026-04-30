82f54aecb70483a82c2a443524de0bd5acbff2e5
Bonolo Lekalakala <mmabosigo058@gmail.com>
2026-04-30 16:26:24 +0200

    Created initial SQL schema for customers, accounts, and transactions tables.

---
+++ database/schema.sql
@@ -0,0 +1,27 @@
+--Core tables
+--Customers (customer_id(PK), name, surname, email)
+--Accounts (accounts_id(PK), customer_id(FK), balance)
+--Transactions (transaction_id(PK), accounts_id(FK), amount, transaction_type, transaction_date)
+
+CREATE TABLE Customers (
+    customer_id INT PRIMARY KEY,
+    name VARCHAR(100),
+    surname VARCHAR(100),
+    email VARCHAR(100)
+);
+
+CREATE TABLE Accounts (
+    account_id INT PRIMARY KEY,
+    customer_id INT,
+    balance DECIMAL(10,2),
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+);
+
+CREATE TABLE Transactions (
+    transaction_id INT PRIMARY KEY,
+    account_id INT,
+    amount DECIMAL(10,2),
+    transaction_type VARCHAR(50),
+    transaction_date DATETIME,
+    FOREIGN KEY (account_id) REFERENCES Accounts(account_id)
+);
